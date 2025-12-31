5fb78393cfa83fd4568c06274e02b1e8e12c6a0c
xRunda <guangguangdang@gmail.com>
2025-12-31 11:24:10 +0800

    RS2026
    成功烧录润思 2026！

--- test_upload_mengm2.sh
+++ test_upload_mengm2.sh
@@ -2,7 +2,7 @@
 # 上传 mengm2.jpg 图片URL到ESP32设备
 
 DEVICE_IP="192.168.1.89"
-IMAGE_URL="https://res.xrunda.com/ai-test/mengm2.jpg"
+IMAGE_URL="http://res.xrunda.com/ai-test/rs2026.jpg"
 
 echo "上传图片URL到设备: $DEVICE_IP"
 echo "图片URL: $IMAGE_URL"
